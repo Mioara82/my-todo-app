81c74bd7325730b2d82edac2862df97e2c1b8cb4
Mioara Cenusa <mioaracenusa82@gmail.com>
2025-04-03 10:08:56 +0100

    temporarily disable todos store due to type errors

--- src/lib/store/todos_store.ts
+++ src/lib/store/todos_store.ts
@@ -1,50 +1,52 @@
-import { createStore } from "zustand";
-import type { Todo } from "../types";
+// import { createStore } from "zustand";
+// import type { Tables } from "../../../database.types";
 
-export type Todos = {
-  todos: Todo[];
-};
+// type Todo = Tables<"todos">;
 
-export type TodosActions = {
-  addTodo: (todo: Todo) => void;
-  deleteTodo: (id: number) => void;
-  toggleTodo: (id: number) => void;
-};
+// export type Todos = {
+//   todos: Todo[];
+// };
 
-export type TodosStore = Todos & TodosActions;
+// export type TodosActions = {
+//   addTodo: (todo: Todo) => void;
+//   deleteTodo: (id: number) => void;
+//   toggleTodo: (id: number) => void;
+// };
 
-export const defaultState: Todos = {
-  todos: [],
-};
+// export type TodosStore = Todos & TodosActions;
 
-export const createTodosStore = (initialState: Todos = defaultState) => {
-  return createStore<TodosStore>()((set) => ({
-    ...initialState,
-    addTodo: (todo: Todo) =>
-      set((state: Todos) => ({
-        todos: [
-          ...state.todos,
-          {
-            value: todo.value,
-            id: todo.id,
-            priority: todo.priority,
-            complexity: todo.complexity,
-            dueDate: todo.dueDate,
-            subtasks: todo.subtasks,
-            tags: todo.tags,
-            isCompleted: todo.isCompleted,
-          },
-        ],
-      })),
-    deleteTodo: (id: number) =>
-      set((state: Todos) => ({
-        todos: state.todos.filter((todo: Todo) => todo.id !== id),
-      })),
-    toggleTodo: (id: number) =>
-      set((state: Todos) => ({
-        todos: state.todos.map((todo: Todo) =>
-          todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
-        ),
-      })),
-  }));
-};
+//             id: todo.id,
+//             priority: todo.priority,
+//             complexity: todo.complexity,
+//             dueDate: todo.due_date,
+//             isCompleted: todo.is_completed,
+//             value: todo.value ?? null,
+//             subtasks: todo.subtasks ?? [],
+//             tags: todo.tags ?? [],
+//       set((state: Todos) => ({
+//         todos: [
+//           ...state.todos,
+//           {
+//             value: todo.value,
+//             id: todo.id,
+//             priority: todo.priority,
+//             complexity: todo.complexity,
+//             dueDate: todo.due_date,
+//             subtasks: todo.subtasks,
+//             tags: todo.tags,
+//             isCompleted: todo.is_completed,
+//           },
+//         ],
+//       })),
+//     deleteTodo: (id: number) =>
+//       set((state: Todos) => ({
+//         todos: state.todos.filter((todo: Todo) => todo.id !== id),
+//       })),
+//     toggleTodo: (id: number) =>
+//       set((state: Todos) => ({
+//         todos: state.todos.map((todo: Todo) =>
+//           todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+//         ),
+//       })),
+//   }));
+// };
